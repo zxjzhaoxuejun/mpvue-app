633fd162b3ff2a6b420e01f639e3cac156d27bf1
赵学军 <zhaoxj@zhongan.com.cn>
2018-10-08 10:03:52 +0800

    修改

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@
 const online = 'https://www.sieia.org/index'
 // 本地地址
 const local = 'http://10.18.204.142:8000'
-const host = online
+const host = local
 const config = {
   host,
   loginUrl: `${host}/weapp/login`,
